5302066f11a809ba61c9e2ebdeacacc09374a0de
Jaxson Pahukula <jaxpahu@gmail.com>
2024-06-22 12:29:43 -0400

    improve cli

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
 
-use std::io::stdin;
-use ansi_term::Colour::Red;
+use rustyline::{history::History, DefaultEditor};
+use ansi_term::Color::Red;
 
 use scientific_calculator::{calculate, context::Context};
 
 fn main() {
-	let mut raw_input: String;
+
 	let mut ctx = Context::new();
+
+	// IO
+	let mut rl = DefaultEditor::new().unwrap();
 	loop {
-		raw_input = String::new();
-		match stdin().read_line(&mut raw_input) {
-			Ok(_) => {}
-			Err(e) => {
-				println!("IO error: {}", e);
+		let readline = rl.readline("");
+		if let Err(_) = &readline {
+			break;
+		}
+		let input = readline.unwrap();
+		let input = input.as_str();
+		if input.eq_ignore_ascii_case("exit") { break; }
+
+		match calculate(input, &mut ctx) {
+			Ok(result) => {
+				println!(" = {result}");
+				rl.history_mut().add(input).expect("Failed to append to history");
+			}
+			Err(e) => { 
+				println!("{}", Red.paint(e.to_string()));
 			}
 		}
-		let input_string = raw_input.trim();
-
-		if input_string.eq("exit") { break; } 
-
-		match calculate(input_string, &mut ctx) {
-			Ok(result) => println!(" = {}", result),
-			Err(e) => println!("{}", Red.paint(e.to_string())),
-		}
 	}
+
+	// bye bye
 }
